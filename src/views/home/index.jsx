042b0cf7086edea0640c3809a3720e0eb34d851b
Noeli <noe.rmorales98@gmail.com>
2023-11-17 13:13:01 -0500

    Refactor code to remove unnecessary framer-motion animation

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -5,36 +5,14 @@ import MyPortfolio from "./components/MyPortfolio";
 import PreguntasFrecuentes from "./components/PreguntasFrecuentes";
 import MyStack from "./components/MyStack";
 
-import { motion, AnimatePresence } from "framer-motion";
-
 export default function Index() {
-  const pageVariants = {
-    initial: { opacity: 0 },
-    in: { opacity: 1 },
-    out: { opacity: 0 },
-  };
-
-  const pageTransition = {
-    type: "tween",
-    ease: "anticipate",
-    duration: 1,
-  };
-
   return (
-    <AnimatePresence>
-      <motion.div
-        initial="initial"
-        animate="in"
-        exit="out"
-        variants={pageVariants}
-        transition={pageTransition}
-      >
-        <Header />
-        <MyStack />
-        <WebAMedida />
-        <MyPortfolio />
-        <PreguntasFrecuentes />
-      </motion.div>
-    </AnimatePresence>
+    <React.Fragment>
+      <Header />
+      <MyStack />
+      <WebAMedida />
+      <MyPortfolio />
+      <PreguntasFrecuentes />
+    </React.Fragment>
   );
 }
